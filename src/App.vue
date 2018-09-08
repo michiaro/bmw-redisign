4ba2c344def2b8dcb411f620c561f108a1bdf2ea
Michiaro <michiaro@bk.ru>
2018-09-09 01:18:48 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+  #app
+    header.header i'm fixed header
+    swiper(
+      :options="swiperOption",
+      ref="mySwiper"
+    )
+      swiper-slide(
+        data-hash="overview"
+      ) i'm overview screen
+      swiper-slide(
+        data-hash="design"
+      ) i'm design screen
+      swiper-slide(
+        data-hash="engine"
+      ) i'm engine screen
+      swiper-slide(
+        data-hash="interior"
+      ) i'm interior screen
+      swiper-slide(
+        data-hash="fulltech"
+      ) i'm full tech screen
+      swiper-slide(
+        data-hash="contact"
+      ) i'm contact screen
+
+      .swiper-pagination(
+        slot="pagination"
+      )
+    footer.footer i'm fixed footer
+    //- <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      swiperOption: {
+        direction: 'vertical',
+        slidesPerView: 1,
+        mousewheel: true,
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+        hashNavigation: {
+          watchState: true,
+        },
+      }
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  mounted() {
+    // this.swiper.slideTo(3, 1000, false);
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+import 'normalize.css';
+import 'swiper/dist/css/swiper.css';
+import '../styles/main.scss';
+
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App.vue'
 
+
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   render: h => h(App)
